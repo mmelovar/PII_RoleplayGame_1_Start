501b53298f4c805b131596defa955219df94c0f5
SebaBerardi <sebastianberardi2@gmail.com>
2026-09-14 20:03:01 -0300

    Update Item.cs

--- src/Library/Items/Item.cs
+++ src/Library/Items/Item.cs
@@ -4,7 +4,7 @@
 // </copyright>
 //--------------------------------------------------------------------------------
 
-namespace Ucu.Poo.RolePlayGame
+namespace Library.Items.RolePlayGame
 {
     /// <summary>
     /// Representa un elemento o equipamiento del juego.
