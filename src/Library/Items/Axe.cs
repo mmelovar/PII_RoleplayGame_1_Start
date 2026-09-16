b16e39dd1c05785f6f58bc01ebb6de14dc905584
Sebastián Berardi <sebastianberardi2@gmail.com>
2026-09-16 13:05:56 -0300

    Dwarf, Axe y Shield Update

--- src/Library/Items/Axe.cs
+++ src/Library/Items/Axe.cs
@@ -1,10 +1,27 @@
+//--------------------------------------------------------------------------------
+// <copyright file="Axe.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
 using System;
 namespace Library.Items
 {
+    /// <summary>
+    /// Representa un hacha (Axe) que puede ser utilizada por los enanos.
+    /// </summary>
     public class Axe : Item
     {
+    /// <summary>
+    /// Inicializa una nueva instancia de la clase <see cref="Axe"/>.
+    /// </summary>
+    /// <param name="name">El nombre del hacha.</param>
+    /// <param name="attackValue">El valor de ataque del hacha.</param>
+    /// <param name="defenseValue">El valor de defensa del hacha.</param>
+    
         public Axe(string name, int attackValue, int defenseValue) : base("Axe", name, attackValue, defenseValue)
         {
         }
     }
 }
+
